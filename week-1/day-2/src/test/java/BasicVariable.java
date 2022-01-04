54962e94c0002247ca80aaa3f9cb6241ecc9a09a
Rehithap <rehitha.paimalagari@collabera.com>
2022-01-04 12:49:30 +0530

    java BasicVariable

---
+++ week-1/day-2/src/test/java/BasicVariable.java
@@ -0,0 +1,14 @@
+public class BasicVariable {
+    int age=10;
+    float Sal = 20.6f;
+    double annualSal= 56.56;
+    char ch ='a';
+    boolean Manager = false;
+    byte byt  =10;
+    short sh =10;
+    long lng = 464646;
+    String nm = "android";
+}
+public void TestCase() {
+}
+}
