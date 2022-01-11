951216950687879d824edbc9513376273f61cc51
Rehithap <rehitha.paimalagari@collabera.com>
2022-01-11 15:52:22 +0530

    spring mvc : mvc crud demo

---
+++ week-2/spring-mvc/mvc-demo/mvc-demo/src/main/java/com/ani/mvcdemo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.ani.mvcdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class HelloController {
+
+    @GetMapping("/login")
+    public String login() {
+
+        return "login";
+    }
+}
